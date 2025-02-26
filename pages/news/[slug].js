090f62b67b17e866e69346e9bb3563bae7d278df
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-26 11:16:15 +0300

    feat: extract dynamic route params

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -1,7 +1,12 @@
+import { useRouter } from "next/router"
+
 const NewsDetail = () => {
+    const router =  useRouter()
+    const slug = router.query.slug
     return (
         <>
             <h1> Details page</h1>
+            <p>{slug}</p>
         </>
     )
 }
