9ce5688087831077bf4d42971e4eeb3920d91872
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-26 12:19:09 +0300

    style: add site internal links with Link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link"
+
 const NewsPage = () => {
     return (
         <>
             <h1>News Page</h1>
+            <ul>
+                <li>
+                    <Link href='/news/next-js-is-awesome'>
+                        next js is awesome
+                    </Link> 
+                </li>
+                <li>
+                    <Link href='/news/next-js-is-optimised'>
+                        next js is optimised
+                    </Link> 
+                </li>
+            </ul>
         </>
     )
 }
